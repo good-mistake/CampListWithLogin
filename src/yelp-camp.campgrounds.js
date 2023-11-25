ce751f6a1196b5ee7372abd375393399f262625e
good-mistake <devillmk88@gmail.com>
2023-11-26 02:41:22 +0330

    firs one

---
+++ src/yelp-camp.campgrounds.js
@@ -0,0 +1,647 @@
+export const data = [
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf860d1",
+    },
+    title: "Ocean Group Camp",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Miami Beach, Florida",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf860d3",
+    },
+    title: "Forest Backcountry",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Keizer, Oregon",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf860d5",
+    },
+    title: "Elk Pond",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Bolingbrook, Illinois",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf860d7",
+    },
+    title: "Maple Bay",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Davenport, Iowa",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf860d9",
+    },
+    title: "Grizzly Canyon",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Hagerstown, Maryland",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf860db",
+    },
+    title: "Tumbling Hunting Camp",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "San Rafael, California",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf860dd",
+    },
+    title: "Roaring Creekside",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Cutler Bay, Florida",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf860df",
+    },
+    title: "Roaring Cliffs",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Hamilton, Ohio",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf860e1",
+    },
+    title: "Roaring Canyon",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Santa Maria, California",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf860e3",
+    },
+    title: "Diamond Camp",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Erie, Pennsylvania",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf860e5",
+    },
+    title: "Redwood Canyon",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Paramount, California",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf860e7",
+    },
+    title: "Dusty Camp",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Yuma, Arizona",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf860e9",
+    },
+    title: "Misty Backcountry",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Huber Heights, Ohio",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf860eb",
+    },
+    title: "Forest Mule Camp",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Palatine, Illinois",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf860ed",
+    },
+    title: "Misty Spring",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Beloit, Wisconsin",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf860ef",
+    },
+    title: "Sky Spring",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Simi Valley, California",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf860f1",
+    },
+    title: "Sea Cliffs",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Stanton, California",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf860f3",
+    },
+    title: "Bullfrog Backcountry",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Avondale, Arizona",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf860f5",
+    },
+    title: "Forest Mule Camp",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Beaumont, California",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf860f7",
+    },
+    title: "Roaring River",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Huntington Park, California",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf860f9",
+    },
+    title: "Grizzly Backcountry",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Mission Viejo, California",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf860fb",
+    },
+    title: "Ancient Hunting Camp",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Woburn, Massachusetts",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf860fd",
+    },
+    title: "Petrified Camp",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Kent, Washington",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf860ff",
+    },
+    title: "Sky Group Camp",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Wilmington, Delaware",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf86101",
+    },
+    title: "Sky Pond",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Dearborn Heights, Michigan",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf86103",
+    },
+    title: "Forest Spring",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Brookhaven, Georgia",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf86105",
+    },
+    title: "Ocean Bay",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Anderson, Indiana",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf86107",
+    },
+    title: "Elk Flats",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Sherman, Texas",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf86109",
+    },
+    title: "Grizzly Creek",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Scottsdale, Arizona",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf8610b",
+    },
+    title: "Misty Camp",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Santa Ana, California",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf8610d",
+    },
+    title: "Forest Spring",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Plainfield, Illinois",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf8610f",
+    },
+    title: "Silent Creekside",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Perris, California",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf86111",
+    },
+    title: "Misty Spring",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "San Diego, California",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf86113",
+    },
+    title: "Grizzly Hollow",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Clovis, New Mexico",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf86115",
+    },
+    title: "Sea Bayshore",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Flagstaff, Arizona",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf86117",
+    },
+    title: "Dusty Pond",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Lancaster, California",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf86119",
+    },
+    title: "Grizzly Bayshore",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Livonia, Michigan",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf8611b",
+    },
+    title: "Elk Backcountry",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Lexington-Fayette, Kentucky",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf8611d",
+    },
+    title: "Sea Creekside",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Boca Raton, Florida",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf8611f",
+    },
+    title: "Petrified Spring",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Kettering, Ohio",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf86121",
+    },
+    title: "Misty Backcountry",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "The Colony, Texas",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf86123",
+    },
+    title: "Forest Flats",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Boise City, Idaho",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf86125",
+    },
+    title: "Dusty Flats",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Centennial, Colorado",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf86127",
+    },
+    title: "Ancient Sands",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Sioux Falls, South Dakota",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf86129",
+    },
+    title: "Diamond Flats",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Honolulu, Hawaii",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf8612b",
+    },
+    title: "Grizzly Spring",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "San Rafael, California",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf8612d",
+    },
+    title: "Misty Camp",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Frisco, Texas",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf8612f",
+    },
+    title: "Sky Hollow",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Champaign, Illinois",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf86131",
+    },
+    title: "Forest Ghost Town",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Warren, Michigan",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b56ec2604d43c5daf86133",
+    },
+    title: "Petrified Creek",
+    image: "https://source.unsplash.com/collection/483251",
+    price: 21,
+    description:
+      "Lorem Ipsum masdasdasdsaqwdwqddwq qddqw dqdqwd qwdqwdwq dqwdqwdwqdwq dqwdwqdw",
+    location: "Franklin, Tennessee",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b6bf12409dac35213f0661",
+    },
+    title: "Red River Gorge",
+    image: "https://source.unsplash.com/collection/483251",
+    description:
+      "We were camping on a ridge in Red River Gorge and when the stars came out we couldn’t help ourselves.\r\n\r\n",
+    location: "Red River Gorge",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b6c8fde3d3965dfe05ce56",
+    },
+    title: "sqs",
+    image:
+      "https://images.unsplash.com/photo-1507668339897-8a035aa9527d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1365&q=80",
+    description: "dwqdwq qwqdqw",
+    location: "sqsq",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64b6cac5e3d3965dfe05ce5a",
+    },
+    title: "qeedq",
+    image:
+      "https://images.unsplash.com/photo-1507668339897-8a035aa9527d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1365&q=80",
+    price: 111,
+    description: "qdqwdqwdqw",
+    location: "eqeq",
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: "64beaa52fa9868306bbc63a7",
+    },
+    title: "sqsq",
+    image: "sqs",
+    price: null,
+    description: "",
+    location: "",
+    __v: 0,
+  },
+];
